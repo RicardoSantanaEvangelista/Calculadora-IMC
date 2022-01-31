66fefdb82b60b5ad13f68a3ce9dc782281611423
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-31 12:40:26 -0300

    Adicioando e melhorando a estilização dos inputs e criando a função para calcular o IMC

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App (){
+
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
+
+  function calcularIMC (){
+    alert("Clicou")
+  }
+
+  return(
+   <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>Vamos Calcular seu IMC ?</span>
+
+      <div className="area-input">
+          <input 
+            type="text"
+            placeholder="Peso em [KG] Ex: 75"
+            value={peso}
+            onChange={ (e) => setPeso(e.target.value) }
+          />
+
+          <input 
+            type="text"
+            placeholder="Altura em [CM] Ex: 170"
+            value={altura}
+            onChange={ (e) => setAltura(e.target.value) }
+          />
+
+          <button onClick={calcularIMC}>
+            Calcular
+          </button>
+
+          <h2>Seu IMC foi 25, você está abaixo do peso !</h2>
+      </div>
+   </div>
   );
 }
 
